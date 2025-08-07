f9ce5e83c75f1b9fcf912b1aa006384c7c6d9462
mhaidara972 <mhaidar2@umbc.edu>
2025-08-07 18:43:48 -0400

    Fixed curricular ranges in APP.js

--- src/App.js
+++ src/App.js
@@ -66,18 +66,18 @@ const binaryFields = [
 ];
 
 const dropdownRangeFields = {
-  "Curricular units 1st sem (credited)": [0, 1, 2, 3, 4, 5, 6],
-  "Curricular units 1st sem (enrolled)": [0, 1, 2, 3, 4, 5, 6, 7],
-  "Curricular units 1st sem (evaluations)": [0, 1, 2, 3, 4, 5, 6, 7],
-  "Curricular units 1st sem (approved)": [0, 1, 2, 3, 4, 5, 6, 7],
+  "Curricular units 1st sem (credited)": [0, 1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+  "Curricular units 1st sem (enrolled)": [0, 1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26],
+  "Curricular units 1st sem (evaluations)": [0, 1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45],
+  "Curricular units 1st sem (approved)": [0, 1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26],
   "Curricular units 1st sem (grade)": [0, 5, 10, 12, 14, 16, 18, 20],
-  "Curricular units 1st sem (without evaluations)": [0, 1, 2, 3],
-  "Curricular units 2nd sem (credited)": [0, 1, 2, 3, 4, 5, 6],
-  "Curricular units 2nd sem (enrolled)": [0, 1, 2, 3, 4, 5, 6, 7],
-  "Curricular units 2nd sem (evaluations)": [0, 1, 2, 3, 4, 5, 6, 7],
-  "Curricular units 2nd sem (approved)": [0, 1, 2, 3, 4, 5, 6, 7],
+  "Curricular units 1st sem (without evaluations)": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12],
+  "Curricular units 2nd sem (credited)": [0, 1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+  "Curricular units 2nd sem (enrolled)": [0, 1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26],
+  "Curricular units 2nd sem (evaluations)": [0, 1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45],
+  "Curricular units 2nd sem (approved)": [0, 1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26],
   "Curricular units 2nd sem (grade)": [0, 5, 10, 12, 14, 16, 18, 20],
-  "Curricular units 2nd sem (without evaluations)": [0, 1, 2, 3]
+  "Curricular units 2nd sem (without evaluations)": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12],
 };
 
 export default function App() {
